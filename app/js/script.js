089ff3f5a7a78132a5e779ac4ef0e77ce924735f
Emily Marro <emily.marro@gmail.com>
2021-02-05 16:40:28 -0500

    hamburger transforms to x on click

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hellooooo world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+    
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open');
+    }
+});
